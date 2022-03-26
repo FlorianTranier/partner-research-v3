2ccecfb5b7f6bc05a4629fe12b1de7ed8e3d0404
Blanymon <tranierflorian@gmail.com>
2022-03-27 00:45:16 +0100

    [feat] setup initial structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello')
+
+import { Client, Intents } from 'discord.js'
+import dotenv from 'dotenv'
+import { deployCommands } from './deploy-commands'
+
+dotenv.config()
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.once('ready', () => {
+  deployCommands()
+  console.log('Ready !')
+})
+
+client.login(process.env.DISCORD_TOKEN)
